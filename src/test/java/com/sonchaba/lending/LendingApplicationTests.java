bee409532f7912d4c8ed725361add8d7219e6e37
shadrack <sonchaba@gmail.com>
2023-06-28 17:36:25 +0300

    initial commit

---
+++ src/test/java/com/sonchaba/lending/LendingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sonchaba.lending;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LendingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
